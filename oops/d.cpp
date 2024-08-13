083dc3c1c7d94a206b6efa19a7a524c5f4dc8f38
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-08-13 22:26:34 +0530

    13-8-24

---
+++ oops/d.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class dharmu
+{
+   public:
+   int a,b,c;
+   void display()
+   {
+    cout<<"enter first number:";
+    cin>>a;
+    cout<<"entet second number:";
+    cin>>b;
+    
+    c = a -b;
+    cout<<"the result: "<<c;
+
+   }
+};
+
+int main()
+{
+    dharmu P;
+    P.display();
+    return 0;
+}
