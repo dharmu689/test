48d2b3d9f29391435cc315cc4f203b8d437b5028
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-08-15 21:14:09 +0530

    .

--- DSA/binery_search.cpp
+++ DSA/binery_search.cpp
@@ -24,24 +24,19 @@ int main()
          mid = (top+bottom)/2;
          
         if(arr[mid] == item)
-        {
-           result = 0;
-        }
+       {
+          result = mid;
+       }
         else if(arr[mid] < item)
-        {
-           top = mid + 1;
-        }
-        else
-          bottom = mid -1;
 
-    }
-    if(result == 0)
-    {
-        cout<<"element found at index: "<<result;
-    }
-    else
-    {
-        cout<<"item not found.";
-    }
+        top = mid +1;
+
+        else
+        bottom = mid - 1;
+
+     }
+
+     (result != -1) ? cout<<"element found at index: "<<mid : cout<<"Element not found";
+   
     return 0;
 }
