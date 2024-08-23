a3e8a1142001567c935464d03bbb011a3a9c05b8
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-08-23 19:40:52 +0530

    23-8-24

---
+++ oops/string.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+
+string s = ("Hello");
+
+cout <<s;
+
+return 0;
+
+}
---
+++ oops/string2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class ABC
+{
+public:
+    string s1 = "Hello";
+    string s2 = "world";
+
+    string s3 = s1 + " " + s2; 
+
+    ABC()
+    {
+        s3.push_back('!'); 
+
+    }
+
+    
+    void display()
+    {
+        cout << s3 << endl; 
+    
+    }
+};
+
+int main()
+{
+    ABC a;
+    a.display();
+    return 0;
+}
