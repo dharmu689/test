eec2598ba8d5f9b50ec68bfe420edecd12b4c5d2
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-09-01 11:48:08 +0530

    1-9-24

---
+++ DSA/Traversal.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    int n = 5; // Number of elements
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
