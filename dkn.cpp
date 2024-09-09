b2a60b8440f1813e8570a5d8c5a9aed70b85ca7d
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-09-09 21:59:20 +0530

    ..

---
+++ dkn.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 62, y = 62;
+
+    if (x > y)
+
+        cout << "x is greater";
+
+    else if (y > x)
+
+        cout << "y is greater";
+        
+    else 
+        cout << "both are equal";
+}
