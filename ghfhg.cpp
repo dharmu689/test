81982a5e22436ad5a92aa61e3ab427ff40642aa2
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-09-10 21:18:48 +0530

    .

---
+++ ghfhg.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num=1,n;
+    cin>>n;
+    while(num<=n)
+    {
+        cout<<num*num<<" ";
+        num++;
+
+    }
+    return 0;
+}
