f5011a16d9870219c0f9f412a3182d8c8a9ef34a
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-01 21:13:23 +0530

    ;;

--- DSA/lab_1.cpp
+++ DSA/lab_1.cpp
@@ -5,11 +5,13 @@ int main()
     int arr[5],i,element;
     
     cout<<"Enter 4 array elements: ";
+
     for(i=0;i<4;i++)
+    {
     cin>>arr[i];
     cout<<"Enter the element to be listed: ";
     cin>>element;
-
+    }
     arr[i] = element;
     cout<<"The new array is: ";
     for(i=0;i<5;i++)
