5391e7dc25503712476c2febc605326207b5f539
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-04 22:17:50 +0530

    .

---
+++ 2.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int i = 5; //Initialization
+    
+    while(i<=20){
+        cout << i << endl;
+        i = i+1; //Update statement
+    }
+}
