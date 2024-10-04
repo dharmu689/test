7b1abf17d7e9db1b5aed4eafc68b459709c78d0f
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-04 22:11:44 +0530

    4-10-24

---
+++ oops/1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+ {
+
+  int a = 0;
+  // Remove the underscores and complete the code
+  while (a < 7 ) {
+    cout << a << endl;
+    a = a + 1;
+  }
+  return 0;
+}
