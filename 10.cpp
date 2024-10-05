34cb9065d68b246cf5cb29f24a54becca99b2bd6
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-06 01:08:39 +0530

    ..

--- 10.cpp
+++ 10.cpp
@@ -2,21 +2,25 @@
 using namespace std;
 
 // User-defined function to generate and print a list of numbers from 1 to limit
-void list_generator(int limit) {
+void list_generator(int limit) 
     // Update the code below to solve the problem
-    int square(int num)
+   
     {
-        return num*num;
+        for(int i = 1;i<=limit;i++)
+        {
+        cout<<i<<" ";
+        }
+        cout<<endl;
     }
-    
-    
-}
 
 // Do not update the code below this line
 
 int main() {
     int A, B;
-    cin >> A >> B;
+    cout<<"1n: ";
+    cin >> A ;
+    cout<<"2n: ";
+    cin>> B;
 
     list_generator(A);
     list_generator(B);
