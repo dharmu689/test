924dbf148197de2ebc32be6c42a7c77f368add76
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-06 00:53:07 +0530

    .

---
+++ 10.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// User-defined function to generate and print a list of numbers from 1 to limit
+void list_generator(int limit) {
+    // Update the code below to solve the problem
+    int square(int num)
+    {
+        return num*num;
+    }
+    
+    
+}
+
+// Do not update the code below this line
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+
+    list_generator(A);
+    list_generator(B);
+
+    
+}
