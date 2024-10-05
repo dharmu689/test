bbfc9ec27465d8206e88340f20049d2410b3ee0d
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-05 23:30:28 +0530

    ..

---
+++ 6.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b,c,d;
+    cout<<a<<b;
+	cin>>a>>b;
+	
+	c = a*a+2*a*b+b*b;
+	d = a+b;
+	
+	cout<<c<<endl;
+	cout<<d<<endl;
+	
+	cin>>a>>b;
+	c = a*a+2*a*b+b*b;
+	d = a+b;
+	cout<<c<<endl;
+	cout<<d<<endl;
+	
+	cin>>a>>b;
+	c = a*a+2*a*b+b*b;
+	d = a+b;
+	cout<<c<<endl;
+	cout<<d<<endl;
+	
+	
+
+}
