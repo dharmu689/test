c72bd3e6e0a89729ffd949a21a9371cb8c44369b
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-12 08:06:42 +0530

    ..

--- DSA/Lab_13.cpp
+++ DSA/Lab_13.cpp
@@ -1,22 +1,27 @@
 #include <iostream> 
 using namespace std;
 
-struct QNode { int data; QNode* next;QNode(int d)
-{
-data = d;
-next = NULL;
-}
+struct QNode 
+{ 
+    int data; 
+    QNode* next;
+    QNode(int d)
+  {
+    data = d;  
+    next = NULL;
+  }  
 };
 
 struct Queue 
 {
-QNode *front, *rear;
-Queue()
-{
-front = rear = NULL;
-}
+   QNode *front, *rear;
 
-void enQueue(int x)
+   Queue()
+  {
+     front = rear = NULL;
+  }
+
+ void enQueue(int x)
 {
 
 // Create a new LL 
@@ -29,33 +34,31 @@ if (rear == NULL)
 front = rear = temp;
 return;
 }
-
 // Add the new node at
 // the end of queue and change rear
-rear->next = temp;
+rear->next = temp; 
 rear = temp;
 }
 
-// Function to remove
-// a key from given queue q 
-void deQueue()
-{
-// If queue is empty, return NULL.
-if (front == NULL)
-return;
+  // Function to remove
+  // a key from given queue q 
+ void deQueue()
+ {
+  // If queue is empty, return NULL.
+  if (front == NULL)
+  return;
 
-// Store previous front and
-// move front one node ahead
-QNode* temp = front;
-front = front->next;
+ // Store previous front and
+ // move front one node ahead
+ QNode* temp = front; 
+ front = front->next;
 
-// If front becomes NULL, then
-// change rear also as NULL
-if (front == NULL)
-rear = NULL;
-
-delete (temp);
-}
+ // If front becomes NULL, then
+ // change rear also as NULL
+ if (front == NULL)
+ rear = NULL; 
+ delete (temp);
+ } 
 };
 
 // Driven Program 
@@ -63,14 +66,15 @@ int main()
 {
 
 Queue q;  
-q.enQueue(10);
+q.enQueue(10); 
 q.enQueue(20);
-q.deQueue();
+q.deQueue(); 
 q.deQueue(); 
 q.enQueue(30); 
 q.enQueue(40);
 q.enQueue(50); 
 q.deQueue();
-cout<< "Queue Front : " << (q.front)->data <<endl; cout<< "Queue Rear : " << (q.rear)->data;
+cout<< "Queue Front : " << (q.front)->data <<endl; 
+cout<< "Queue Rear : " << (q.rear)->data;
 return 0;
 }
