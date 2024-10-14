d4e59857b307e157db0a97c9e5cf276feef478a5
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-14 20:16:01 +0530

    14-10-24

---
+++ oops/Lab_16.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<<"Enter the value of a and b: "<<endl;
+    cout<<"a = ";
+    cin>>a;
+    cout<<endl;
+    cout<<"b = ";
+    cin>>b;
+    c = a-b;
+    try
+    {
+        if(c!=0)
+        {
+            cout<<"The result of a-b is = "<<c<<endl;
+        }
+        else
+        {
+           throw(c);
+        }
+    }
+    catch(int x)
+    {
+        cout<<"exeption has occured: "<<c;
+    }
+    return 0;
+    
+}
