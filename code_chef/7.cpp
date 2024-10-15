dfdf8e71b94c2e71d7539374ae7348adcba498df
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-15 18:56:58 +0530

    ..

--- code_chef/7.cpp
+++ code_chef/7.cpp
@@ -1,11 +1,16 @@
 #include<iostream>
 using namespace std;
 
+int area(int a, int b)
+{
+	return a*a+b*b+2*a*b;
+}
+
 int main() 
 {
 	// your code goes here
 	
-	int a,b,c,d;
+	/*int a,b,c,d;
 	cin>>a>>b;
 	
 	c = a*a+2*a*b+b*b;
@@ -24,8 +29,18 @@ int main()
 	c = a*a+2*a*b+b*b;
 	d = a+b;
 	cout<<c<<endl;
-	cout<<d<<endl;
+	cout<<d<<endl;*/
 	
+	int num1, num2;
+
+	cout<<"1 no: ";
+	cin>>num1;
 	
+	cout<<"2 no: ";
+	cin>>num2;
+
+	int result1 = area(num1,num2);
+
+	cout<<"area of a and b : " << result1;
 
 }
