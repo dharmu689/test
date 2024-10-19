383db74b8aeec57e4bbfdcca4f0d6f8573f01ea8
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-19 09:08:59 +0530

    m

---
+++ code_chef/upto 500/4.cpp
@@ -0,0 +1,18 @@
+//Double rent
+
+//Question - https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DOUBLERENT
+
+//Solution - https://www.codechef.com/viewsolution/1099512834
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+	// your code goes here
+	int X;
+	cin>>X;
+	
+	cout<< 2*X;
+
+}
