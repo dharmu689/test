c231766053c4f92151017a6a7c11fd7ecf211af1
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-19 09:09:22 +0530

    Create 5.cpp

---
+++ code_chef/upto 500/5.cpp
@@ -0,0 +1,25 @@
+//Saving Taxes
+
+//Question - https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TAXSAVING
+
+//Solution - https://www.codechef.com/viewsolution/1099515508
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int X,Y;
+	cin>>X>>Y;
+	
+	cout<<X-Y<<endl;;
+	}
+
+}
