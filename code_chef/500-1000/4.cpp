fe871a1785ee3eef603c22300fd9664f9057f5e4
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-20 11:29:42 +0530

    Create 4.cpp

---
+++ code_chef/500-1000/4.cpp
@@ -0,0 +1,20 @@
+//Question - https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DETSCORE
+//solution - https://www.codechef.com/viewsolution/1099659819
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+        int a = x / 10;
+        cout << a * y << endl;
+    }
+
+}
