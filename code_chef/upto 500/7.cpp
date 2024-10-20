4ddf2b7f3aa0a7f6389403a8103eedf567bbe5f4
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-20 11:29:56 +0530

    Create 7.cpp

---
+++ code_chef/upto 500/7.cpp
@@ -0,0 +1,26 @@
+Question - https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/SUBSCRIBE_?tab=statement
+Solution - https://www.codechef.com/viewsolution/1099730847
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,x;
+	    cin>>n>>x;
+	   int y = n/6;
+	   if(n>y*6)
+	   {
+	       cout<<(y+1)*x<<endl;
+	   }
+	   else
+	   {
+	       cout<<y*x<<endl;
+	   }
+	}
+
+}
