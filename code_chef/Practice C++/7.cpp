1db7a53285d142520511d08c14d884e2a6291a09
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-20 11:30:07 +0530

    Update 7.cpp

--- code_chef/Practice C++/7.cpp
+++ code_chef/Practice C++/7.cpp
@@ -1,47 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-int area(int a, int b)
+int main()
 {
-	return a*a+b*b+2*a*b;
-}
-
-int main() 
-{
-	// your code goes here
-	
-	/*int a,b,c,d;
-	cin>>a>>b;
-	
-	c = a*a+2*a*b+b*b;
-	d = a+b;
-	
-	cout<<c<<endl;
-	cout<<d<<endl;
-	
-	cin>>a>>b;
-	c = a*a+2*a*b+b*b;
-	d = a+b;
-	cout<<c<<endl;
-	cout<<d<<endl;
-	
-	cin>>a>>b;
-	c = a*a+2*a*b+b*b;
-	d = a+b;
-	cout<<c<<endl;
-	cout<<d<<endl;*/
-	
-	int num1, num2 ,t;
-
-	cout<<"1 no: ";
-	cin>>num1;
-	
-	cout<<"2 no: ";
-	cin>>num2;
-
-	int result1 = area(num1,num2);
-
-	cout<<"area of a and b : " << result1;
-	
-      return 0;
+    int num;
+    int fact = 1;
+	cout<<"num: ";
+    cin >> num;
+    for(int a = 1; a <= num; a++)
+    {
+        fact = fact*a;
+    }
+	cout<<"The factorial of the given number is: "<<fact<<endl;
+    return 0;
 }
