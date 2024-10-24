d4794fb9eec4449137b95897553ed5fb497adb45
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-24 19:52:30 +0530

    22

--- oops/1.cpp
+++ oops/1.cpp
@@ -1,16 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main()
- {
+int main() {
+    int x;
+    cin >> x;
+    cout << "before try: " << endl;
 
-  int a = 0;
-  
-  // Remove the underscores and complete the code
-  while (a < 7 ) 
-  {
-    cout << a << endl;
-    a = a + 1;
-  }
-  return 0;
+    try {
+        cout << "after try: " << endl;
+        if (x < 0) {
+            throw x;
+            // This line will never be executed because throw x will transfer control to the catch block
+            cout << "after throw: " << endl;
+        }
+    } catch (int x) {
+        cout << "x = " << x << endl;
+        cout << "exception caught: " << endl;
+    }
+
+    cout << "after catch will execute: " << endl;
+    return 0;
 }
