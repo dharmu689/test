7754ce9165b2c6faef708fa43d784a2621e60db2
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-24 19:52:34 +0530

    Update 1.cpp

--- oops/Class/1.cpp
+++ oops/Class/1.cpp
@@ -7,16 +7,20 @@ int main()
         int num = 10;
         int demo;
         int result;
+        cout<<"number: ";
+        cin>>demo;
         if(demo==0)
         {
             throw runtime_error("division by zero not allowed......!");
+        }
             result = num/demo;
             cout<<"result is: "<<result;
-        }
+        
     }
     catch(exception & e)
     {
-        cout<<"exception is" << e.what() << '\n';
+        cout<<"exception is " << e.what() << '\n';
     }
+    return 0;
     
 }
