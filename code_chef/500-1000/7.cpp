e838f527b7f4df2c3b335e12bf1d17c43be9a0aa
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-25 19:51:47 +0530

    Create 7.cpp

---
+++ code_chef/500-1000/7.cpp
@@ -0,0 +1,18 @@
+//Question - https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CHEFONDATE
+//Solution - https://www.codechef.com/viewsolution/1101250512
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    cout<<4*x+y<<endl;
+	}
+
+}
