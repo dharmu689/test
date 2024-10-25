77d4beb2d2f8de344e9260c3cbd63ca1f99f54ff
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-25 19:51:41 +0530

    Create 11.cpp

---
+++ code_chef/upto 500/11.cpp
@@ -0,0 +1,25 @@
+//Question - https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PRIZEPOOL
+//Solution - https://www.codechef.com/viewsolution/1101258905
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    if(x>=y)
+	    {
+	        cout<<x*10+y*90<<endl;
+	    }
+	    else
+	    {
+	        cout<<x*10+y*10<<endl;
+	    }
+	}
+
+}
