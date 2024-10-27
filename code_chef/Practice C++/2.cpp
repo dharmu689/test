b88e3f2329ddf893b55da107d87314371047f485
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-27 20:41:43 +0530

    Update 2.cpp

--- code_chef/Practice C++/2.cpp
+++ code_chef/Practice C++/2.cpp
@@ -5,7 +5,8 @@ int main()
 {
     // Remove the underscores and complete the code
     int n=1;
-    while (n<=5) {
+    while (n<=5) 
+    {
         cout << n <<"-" <<n*n << endl;
         n++;
     }
