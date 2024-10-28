03dc7baf95a1a587343f0f86dd58898968530e85
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-28 23:22:59 +0530

    Create 319.cpp

---
+++ code_chef/upto 500/319.cpp
@@ -0,0 +1,26 @@
+//Question - https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/OCTATHON
+
+//Solution - https://www.codechef.com/viewsolution/1102298878
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	if(x<3)
+	{
+	    cout<<"GOLD"<<endl;
+	}
+	else if(x>=3 && x<6)
+	{
+	    cout<<"SILVER"<<endl;
+	}
+	else if(x>=6)
+	{
+	    cout<<"BRONZE"<<endl;
+	}
+	
+  return 0;
+}
