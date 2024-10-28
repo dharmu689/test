fce722b4ea8de3e39ad7f5f733ce0eb633dfdc4b
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-28 23:00:41 +0530

    Create 533.cpp

---
+++ code_chef/500-1000/533.cpp
@@ -0,0 +1,40 @@
+//Question - https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/AMR15A?tab=statement
+//Solution - https://www.codechef.com/viewsolution/1101997616
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int n, lucky = 0, unlucky = 0, i, j;
+    cin >> n;
+    int a[n];
+    for (i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (j = 0; j < n; j++)
+    {
+        if (a[j] % 2 == 0)
+        {
+            lucky++;
+        }
+        else
+        {
+            unlucky++;
+        }
+
+    }
+    if (lucky > unlucky)
+    {
+        cout << "READY FOR BATTLE";
+    }
+    else
+    {
+        cout << "NOT READY";
+    }
+
+
+
+}
