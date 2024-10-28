a2284123cbbbecbd2f302d504cef5dd8730bbe9f
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-28 23:00:52 +0530

    Create 541.cpp

---
+++ code_chef/500-1000/541.cpp
@@ -0,0 +1,26 @@
+//Questtion - https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/WATERFILLING?tab=statement
+//Solution - https://www.codechef.com/viewsolution/1102007406
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int b1, b2, b3;
+        cin >> b1 >> b2 >> b3;
+       if((b1+b2+b3)<=1)
+       {
+           cout<<"water filling time"<<endl;
+       }
+       else
+       {
+           cout<<"not now"<<endl;
+       }
+    }
+
+}
