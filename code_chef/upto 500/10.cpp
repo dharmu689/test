b382e25a0d9a7ab9f0fc77f3a793d860384c0620
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-29 22:53:37 +0530

    Update 10.cpp

--- code_chef/upto 500/10.cpp
+++ code_chef/upto 500/10.cpp
@@ -5,17 +5,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main() 
+{
 	// your code goes here
 	int t;
 	cin>>t;
 	while(t--)
 	{
 	    int n;
-	    cin>>n;
-	    
-	    
-	    
+	    cin>>n; 
 	    if(n%3==0)
 	    {
 	        cout<<"yes"<<endl;
