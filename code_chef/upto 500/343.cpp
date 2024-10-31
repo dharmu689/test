585a96a86d4567627e84e6577f97ebfb59fca171
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-31 19:45:39 +0530

    Create 343.cpp

---
+++ code_chef/upto 500/343.cpp
@@ -0,0 +1,28 @@
+//Question - https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/READPAGES
+//Solution - https://www.codechef.com/viewsolution/1103099804
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x, y;
+        cin >> n >> x >> y;
+        if (n <= (x * y))
+        {
+            cout << "yes" << endl;
+
+        }
+        else
+        {
+            cout << "no" << endl;
+        }
+
+    }
+
+}
