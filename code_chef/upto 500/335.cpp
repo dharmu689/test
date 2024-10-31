0252864308f5332a1108d4e80fbe3960a13a1b90
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-10-31 19:09:13 +0530

    Create 335.cpp

---
+++ code_chef/upto 500/335.cpp
@@ -0,0 +1,29 @@
+//Question - https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FINE
+//Solution - https://www.codechef.com/viewsolution/1102592691
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    if(x<=70)
+	    {
+	        cout<<"0"<<endl;
+	    }
+	    else if(x>70 && x<=100)
+	    {
+	        cout<<"500"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"2000"<<endl;
+	    }
+	}
+
+}
