399a13c7535679f758959544fc031f890b767d3a
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-11-01 19:37:00 +0530

    Create 546.cpp

---
+++ code_chef/500-1000/546.cpp
@@ -0,0 +1,20 @@
+//Question - https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/MINPIZZA?tab=statement
+//Solution - https://www.codechef.com/viewsolution/1103283339
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x;
+        cin >> n >> x;
+        int y = (n * x + 3) / 4;
+
+        cout << y << endl;
+    }
+}
