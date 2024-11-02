1047e3563a3ce58d399d21244cf64ec83c721414
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-11-02 19:15:38 +0530

    Create 561.cpp

---
+++ code_chef/500-1000/561.cpp
@@ -0,0 +1,27 @@
+//Question - https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/EXPERT
+//Solution - https://www.codechef.com/viewsolution/1103481467
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        double x, y;
+        cin >> x >> y;
+        if (y >= (x/2))
+        {
+            cout << "yes" << endl;
+        }
+        else
+        {
+            cout << "no" << endl;
+        }
+    }
+
+
+}
