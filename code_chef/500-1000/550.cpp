9574502f249cfff4aaf00c6817a9075f9e1b4f59
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-11-02 18:58:24 +0530

    2-11-24

---
+++ code_chef/500-1000/550.cpp
@@ -0,0 +1,25 @@
+//Question - https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/CHEFGAMES
+//Solution - https://www.codechef.com/viewsolution/1103476778
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int r1,r2,r3,r4;
+	    cin>>r1>>r2>>r3>>r4;
+	    if((r1+r2+r3+r4)>0)
+	    {
+	        cout<<"out"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"in"<<endl;
+	    }
+	}
+
+}
