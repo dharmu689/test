34d7c60e13d8ce938797ac6ff8caecf458f5271c
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-11-05 21:29:03 +0530

    Create 373.cpp

---
+++ code_chef/upto 500/373.cpp
@@ -0,0 +1,26 @@
+//Question - https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/INTRDSGN
+//Solution - https://www.codechef.com/viewsolution/1104224688
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        if ((x1 + y1) >= (x2 + y2))
+        {
+            cout << x2 + y2 << endl;
+        }
+        else
+        {
+            cout << x1 + y1 << endl;
+        }
+    }
+
+}
