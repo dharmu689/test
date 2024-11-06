1db3c52a15a293293ba5cb45205fdce5ba49cb2f
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-11-06 22:42:24 +0530

    Create 572.cpp

---
+++ code_chef/500-1000/572.cpp
@@ -0,0 +1,27 @@
+//Question - https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/TRUESCORE
+//Solution - https://www.codechef.com/viewsolution/1104237639
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c, d;
+        cin >> a >> b;
+        cin >> c >> d;
+        if ((a <= c) && (b <= d))
+        {
+            cout << "possible" << endl;
+        }
+        else
+        {
+            cout << "impossible" << endl;
+        }
+    }
+
+}
