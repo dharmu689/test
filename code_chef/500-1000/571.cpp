6fd4440fd8fd4dc71e5e57cf1e7c59f471901245
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-11-06 22:42:20 +0530

    Create 571.cpp

---
+++ code_chef/500-1000/571.cpp
@@ -0,0 +1,30 @@
+//Question - https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/TRAVELFAST
+//Solution - https://www.codechef.com/viewsolution/1104228295
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+        if (x > y)
+        {
+            cout << "car" << endl;
+        }
+        else if(x < y)
+        {
+            cout << "bike" << endl;
+        }
+        else
+        {
+            cout << "same" << endl;
+        }
+    }
+
+}
