4ea5f27de9096759c07c34362f01dbc4a401906a
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-11-08 20:23:23 +0530

    Create 390a.cpp

---
+++ code_chef/upto 500/390a.cpp
@@ -0,0 +1,29 @@
+//Question - https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/KITCHENSPICE
+//Solution - https://www.codechef.com/viewsolution/1105270057
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    if(4>x)
+	    {
+	        cout<<"MILD"<<endl;
+	    }
+	    else if(x>=4 && x<7)
+	    {
+	        cout<<"MEDIUM"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"HOT"<<endl;
+	    }
+	}
+
+}
