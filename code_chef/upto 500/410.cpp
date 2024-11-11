d11f5e4dc8cd5e02f6a019aef974e743491759a5
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-11-11 19:35:32 +0530

    Create 410.cpp

---
+++ code_chef/upto 500/410.cpp
@@ -0,0 +1,23 @@
+//Question - https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/HS08TEST?tab=Submissions
+//Solution - https://www.codechef.com/viewsolution/1105970079
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int x;
+    double y;
+    cin >> x;
+    cin >> y;
+    if ((x % 5) == 0 && (y >= x + .50))
+    {
+        cout << (y - x) - (0.50);
+    }
+    else
+    {
+        cout << y;
+    }
+
+}
