eed722783e39862ce75514a2c50a1d36fffaff86
Dharmu kumar <dharmukumar1622003@gmail.com>
2024-11-11 18:56:41 +0530

    11-11-24

---
+++ code_chef/upto 500/408.cpp
@@ -0,0 +1,26 @@
+//Question - https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/INSTAGRAM?tab=statement
+//Solution - https://www.codechef.com/viewsolution/1105730629
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+        if (x > (y * 10))
+        {
+            cout << "yes" << endl;
+        }
+        else
+        {
+            cout << "no" << endl;
+        }
+    }
+
+}
